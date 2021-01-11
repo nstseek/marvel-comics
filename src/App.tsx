bd70c4c6936c3c56c73097e2adec246a0148304c
Rafael Lopes Schneider <rafael.rls70@gmail.com>
2021-01-11 00:08:16 -0300

    Cria base do projeto

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,86 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Particles, { IParticlesParams } from 'react-particles-js';
+import { Redirect, Switch } from 'react-router-dom';
+import Routes from 'routes';
+import './App.scss';
 
-function App() {
+const particlesParam: IParticlesParams = {
+  particles: {
+    number: {
+      value: 150,
+      density: {
+        enable: false,
+        value_area: 800
+      }
+    },
+    opacity: {
+      value: 0.5,
+      random: true,
+      anim: {
+        enable: true,
+        speed: 1,
+        opacity_min: 0.1,
+        sync: false
+      }
+    },
+    size: {
+      value: 1,
+      random: true,
+      anim: {
+        enable: false,
+        speed: 10,
+        size_min: 0.1,
+        sync: false
+      }
+    },
+    line_linked: {
+      enable: false
+    },
+    move: {
+      enable: true,
+      speed: 1,
+      direction: 'none',
+      random: true
+    }
+  },
+  interactivity: {
+    detect_on: 'canvas',
+    events: {
+      onhover: {
+        enable: true,
+        mode: 'repulse'
+      },
+      onclick: {
+        enable: true,
+        mode: 'push'
+      },
+      resize: true
+    },
+    modes: {
+      repulse: {
+        distance: 50,
+        duration: 1
+      },
+      push: {
+        particles_nb: 1,
+        quantity: 5
+      }
+    }
+  },
+  retina_detect: true
+};
+
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Particles className='particles' params={particlesParam} />
+      <div className='main-container'>
+        <Switch>
+          <Redirect to={Routes.Home} />
+        </Switch>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
