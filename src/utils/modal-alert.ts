546f193d685af97827079ca2f6cbdf04369393da
Rafael Lopes Schneider <rafael.rls70@gmail.com>
2021-02-01 01:24:28 -0300

    Adiciona instância do Axios

--- src/utils/modal-alert.ts
+++
@@ -1,7 +0,0 @@
-import { ModalBase } from '@nstseek/react-forms/validators';
-
-const createAlert = (modal: ModalBase) => {
-  alert(`${modal.title}: ${modal.desc}`);
-};
-
-export default createAlert;
