4507396ad2c148f2d1edde5bd8d6a0a3c7eae21e
Rafael Lopes Schneider <rafael.rls70@gmail.com>
2021-02-08 19:07:51 -0300

    Integra app com API e ajusta layout

--- src/components/Comic/Comic.test.tsx
+++ src/components/Comic/Comic.test.tsx
@@ -6,7 +6,7 @@ describe('<Comic />', () => {
   let component;
 
   beforeEach(() => {
-    component = shallow(<Comic />);
+    component = shallow(<Comic comic={{thumbnail: {}} as any} addComic={() => null} removeComic={() => null} />);
   });
 
   test('It should mount', () => {
